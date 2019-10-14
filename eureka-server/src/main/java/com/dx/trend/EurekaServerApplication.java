45510fb10da7430fdbf3bdf292229aa7494b5a82
dx <bumonkey@163.com>
2019-10-14 23:51:03 +0800

    init

--- eureka-server/src/main/java/com/dx/trend/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/dx/trend/EurekaServerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author dx
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
